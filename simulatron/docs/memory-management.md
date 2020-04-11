baf7199f10cc3ebbad68f1bac1f6029479d24e0d
Chris Riches <chrisriches42@gmail.com>
2020-04-12 00:04:08 +0100

    VM: Proper MMU implementation.

--- simulatron/docs/memory-management.md
+++ simulatron/docs/memory-management.md
@@ -9,30 +9,32 @@ The Simulatron uses memory-mapped IO, so various devices can be accessed within 
 
 Physical addresses are mapped as follows. All ranges are inclusive.
 
-|     Addresses    |      Mapping
-| ----------------:| -----------------
-|             0-31 | Interrupt Handlers (32 bytes)
-|            32-63 | Reserved (32 bytes)
-|           64-575 | ROM (512 bytes)
-|         576-2575 | Display Characters (2000 bytes)
-|        2576-4575 | Display Foreground Colours (2000 bytes)
-|        4576-6575 | Display Background Colours (2000 bytes)
-|             6576 | Keyboard Key Buffer (1 byte)
-|             6577 | Keyboard Metadata Buffer (1 byte)
-|        6578-8171 | Reserved (1594 bytes)
-|             8172 | Disk A status (1 byte)
-|        8173-8176 | Disk A blocks available (4 bytes)
-|        8177-8180 | Disk A block address (4 bytes)
-|             8181 | Disk A command register (1 byte)
-|             8182 | Disk B status (1 byte)
-|        8183-8186 | Disk B blocks available (4 bytes)
-|        8187-8190 | Disk B block address (4 bytes)
-|             8191 | Disk B command register (1 byte)
-|       8192-12287 | Disk A data (4096 bytes)
-|      12288-16383 | Disk B data (4096 bytes)
-| 16384-4294967295 | RAM (4,294,950,912 bytes = just under 4 GiB)
+|     Addresses    |                   Mapping                    | Read/Write |
+| ----------------:| -------------------------------------------- | ---------- |
+|             0-31 | Interrupt Handlers (32 bytes)                | Both       |
+|            32-63 | Reserved (32 bytes)                          | Neither    |
+|           64-575 | ROM (512 bytes)                              | Read       |
+|         576-2575 | Display Characters (2000 bytes)              | Write      |
+|        2576-4575 | Display Foreground Colours (2000 bytes)      | Write      |
+|        4576-6575 | Display Background Colours (2000 bytes)      | Write      |
+|             6576 | Keyboard Key Buffer (1 byte)                 | Read       |
+|             6577 | Keyboard Metadata Buffer (1 byte)            | Read       |
+|        6578-8171 | Reserved (1594 bytes)                        | Neither    |
+|             8172 | Disk A status (1 byte)                       | Read       |
+|        8173-8176 | Disk A blocks available (4 bytes)            | Read       |
+|        8177-8180 | Disk A block address (4 bytes)               | Write      |
+|             8181 | Disk A command register (1 byte)             | Write      |
+|             8182 | Disk B status (1 byte)                       | Read       |
+|        8183-8186 | Disk B blocks available (4 bytes)            | Read       |
+|        8187-8190 | Disk B block address (4 bytes)               | Write      |
+|             8191 | Disk B command register (1 byte)             | Write      |
+|       8192-12287 | Disk A data (4096 bytes)                     | Both       |
+|      12288-16383 | Disk B data (4096 bytes)                     | Both       |
+| 16384-4294967295 | RAM (4,294,950,912 bytes = just under 4 GiB) | Both       |
 
 Note that the first two frames are taken up by various mappings, frames 3 and 4 are Disk A and B data respectively, and all remaining frames are RAM.
+
+If an access of the wrong type is made (e.g. a write to a read-only section), then an illegal operation interrupt will be sent to the CPU.
 
 ## Virtual memory system
 Virtual memory mapping is achieved via two-level hierarchical page table. The kernel should set the Page Directory Pointer Register to point to a valid page table in memory before entering user mode.
--- simulatron/src/mmu.rs
+++ simulatron/src/mmu.rs
@@ -2,12 +2,23 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::sync::mpsc::Sender;
 
-use crate::cpu::INTERRUPT_ILLEGAL_OPERATION;
+use crate::cpu::{INTERRUPT_ILLEGAL_OPERATION, INTERRUPT_PAGE_FAULT};
 use crate::disk::DiskController;
 use crate::display::DisplayController;
 use crate::keyboard::KeyboardController;
 use crate::ram::RAM;
 use crate::rom::ROM;
+
+pub const PAGE_FAULT_INVALID_PAGE: u32 = 0;
+pub const PAGE_FAULT_ILLEGAL_ACCESS: u32 = 1;
+pub const PAGE_FAULT_NOT_PRESENT: u32 = 2;
+pub const PAGE_FAULT_COW: u32 = 3;
+
+enum Intent {
+    Read,
+    Write,
+    Execute,
+}
 
 pub struct MMU {
     interrupt_channel: Sender<u32>,
@@ -17,6 +28,8 @@ pub struct MMU {
     keyboard: Rc<RefCell<KeyboardController>>,
     ram: Rc<RefCell<RAM>>,
     rom: Rc<ROM>,
+    pdpr: u32,  // Page Directory Pointer Register
+    pfsr: u32,  // Page Fault Status Register
 }
 
 impl MMU {
@@ -35,10 +48,74 @@ impl MMU {
             keyboard,
             ram,
             rom,
+            pdpr: 0,
+            pfsr: 0,
         }
     }
 
-    pub fn store_direct(&mut self, address: u32, value: u8) {
+    pub fn set_page_directory_pointer_register(&mut self, value: u32) {
+        self.pdpr = value;
+    }
+
+    pub fn page_fault_status_register(&self) -> u32 {
+        self.pfsr
+    }
+
+    pub fn store_virtual_8(&mut self, address: u32, value: u8) {
+        match self.virtual_to_physical_address(address, Intent::Write) {
+            Some(physical_address) => self.store_physical_8(physical_address, value),
+            None => self.interrupt_channel.send(INTERRUPT_PAGE_FAULT).unwrap()
+        };
+    }
+
+    pub fn store_virtual_16(&mut self, address: u32, value: u16) {
+        match self.virtual_to_physical_address(address, Intent::Write) {
+            Some(physical_address) => self.store_physical_16(physical_address, value),
+            None => self.interrupt_channel.send(INTERRUPT_PAGE_FAULT).unwrap()
+        };
+    }
+
+    pub fn store_virtual_32(&mut self, address: u32, value: u32) {
+        match self.virtual_to_physical_address(address, Intent::Write) {
+            Some(physical_address) => self.store_physical_32(physical_address, value),
+            None => self.interrupt_channel.send(INTERRUPT_PAGE_FAULT).unwrap()
+        };
+    }
+
+    pub fn load_virtual_8(&mut self, address: u32, is_fetch: bool) -> u8 {
+        let intent = if is_fetch {Intent::Execute} else {Intent::Read};
+        match self.virtual_to_physical_address(address, intent) {
+            Some(physical_address) => self.load_physical_8(physical_address),
+            None => {
+                self.interrupt_channel.send(INTERRUPT_PAGE_FAULT).unwrap();
+                0
+            }
+        }
+    }
+
+    pub fn load_virtual_16(&mut self, address: u32, is_fetch: bool) -> u16 {
+        let intent = if is_fetch {Intent::Execute} else {Intent::Read};
+        match self.virtual_to_physical_address(address, intent) {
+            Some(physical_address) => self.load_physical_16(physical_address),
+            None => {
+                self.interrupt_channel.send(INTERRUPT_PAGE_FAULT).unwrap();
+                0
+            }
+        }
+    }
+
+    pub fn load_virtual_32(&mut self, address: u32, is_fetch: bool) -> u32 {
+        let intent = if is_fetch {Intent::Execute} else {Intent::Read};
+        match self.virtual_to_physical_address(address, intent) {
+            Some(physical_address) => self.load_physical_32(physical_address),
+            None => {
+                self.interrupt_channel.send(INTERRUPT_PAGE_FAULT).unwrap();
+                0
+            }
+        }
+    }
+
+    pub fn store_physical_8(&mut self, address: u32, value: u8) {
         macro_rules! reject {
             () => {{self.interrupt_channel.send(INTERRUPT_ILLEGAL_OPERATION).unwrap()}};
         }
@@ -66,7 +143,21 @@ impl MMU {
         }
     }
 
-    pub fn load_direct(&self, address: u32) -> u8 {
+    pub fn store_physical_16(&mut self, address: u32, value: u16) {
+        let [upper, lower] = u16::to_le_bytes(value);
+        self.store_physical_8(address, upper);
+        self.store_physical_8(address + 1, lower);
+    }
+
+    pub fn store_physical_32(&mut self, address: u32, value: u32) {
+        let [upper, upper_mid, lower_mid, lower] = u32::to_le_bytes(value);
+        self.store_physical_8(address, upper);
+        self.store_physical_8(address + 1, upper_mid);
+        self.store_physical_8(address + 2, lower_mid);
+        self.store_physical_8(address + 3, lower);
+    }
+
+    pub fn load_physical_8(&self, address: u32) -> u8 {
         macro_rules! reject {
             () => {{self.interrupt_channel.send(INTERRUPT_ILLEGAL_OPERATION).unwrap()}};
         }
@@ -103,5 +194,65 @@ impl MMU {
         } else {                     // RAM
             self.ram.borrow().load(address - 16384)
         }
+    }
+
+    pub fn load_physical_16(&self, address: u32) -> u16 {
+        let upper = self.load_physical_8(address);
+        let lower = self.load_physical_8(address + 1);
+        u16::from_le_bytes([upper, lower])  // oui oui le baguette
+    }
+
+    pub fn load_physical_32(&self, address: u32) -> u32 {
+        let upper = self.load_physical_8(address);
+        let upper_mid = self.load_physical_8(address + 1);
+        let lower_mid = self.load_physical_8(address + 2);
+        let lower = self.load_physical_8(address + 3);
+        u32::from_le_bytes([upper, upper_mid, lower_mid, lower])
+    }
+
+    fn virtual_to_physical_address(&mut self, virtual_address: u32, intent: Intent) -> Option<u32> {
+        // Find the directory entry.
+        let directory_entry_address = self.pdpr + 4*(virtual_address >> 22); // First 10 bits of v-addr.
+        let directory_entry = self.load_physical_32(directory_entry_address);
+        // Check it's valid.
+        if (directory_entry & 1) == 0 {
+            self.pfsr = PAGE_FAULT_INVALID_PAGE;
+            return None;
+        }
+        // Find the page table entry.
+        let page_table_base = directory_entry & 0xFFFFF000;  // First 20 bits of entry.
+        let page_table_offset = 4*((virtual_address >> 12) & 0x3FF);  // Second 10 bits of v-addr.
+        let page_table_entry = self.load_physical_32(page_table_base + page_table_offset);
+        // Check it's valid.
+        if (page_table_entry & 1) == 0 {
+            self.pfsr = PAGE_FAULT_INVALID_PAGE;
+            return None;
+        }
+        // Check it's present.
+        if (page_table_entry & 2) == 0 {
+            self.pfsr = PAGE_FAULT_NOT_PRESENT;
+            return None;
+        }
+        // Check permissions.
+        let legal = match intent {
+            Intent::Read => page_table_entry & 4,
+            Intent::Write => page_table_entry & 8,
+            Intent::Execute => page_table_entry & 16,
+        };
+        if legal == 0 {
+            self.pfsr = PAGE_FAULT_ILLEGAL_ACCESS;
+            return None;
+        }
+        // Check COW.
+        if let Intent::Write = intent {
+            if (page_table_entry & 32) == 1 {
+                self.pfsr = PAGE_FAULT_COW;
+                return None;
+            }
+        }
+        // It's allowed, so find the physical address.
+        let frame = page_table_entry & 0xFFFFF000;
+        let frame_offset = virtual_address & 0xFFF;
+        Some(frame | frame_offset)
     }
 }
