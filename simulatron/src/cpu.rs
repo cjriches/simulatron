5652114859d1f1309bd000a2ecc76fb3955c4490
Chris Riches <chrisriches42@gmail.com>
2021-08-18 22:16:06 +0100

    VM: Implemented SREM and UREM.

--- simulatron/src/cpu.rs
+++ simulatron/src/cpu.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Add, Div, Mul, Rem, Sub};
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
@@ -362,7 +362,7 @@ macro_rules! bin_op {
 
 // A macro for simple unsigned binary operations that don't apply to floats.
 macro_rules! bin_op_int {
-    ($self:expr, $reg_ref:expr, $value:expr, $int_op:ident, $float_op:ident) => {{
+    ($self:expr, $reg_ref:expr, $value:expr, $int_op:ident) => {{
         let flags: u16;
         match $self.read_from_register($reg_ref)? {
             TypedValue::Byte(x) => {
@@ -919,7 +919,7 @@ impl<D: DiskController> CPUInternal<D> {
                 self.instruction_sub(reg_ref, value)?;
             }
             0x26 => {  // SUB ref
-            debug!("SUB ref");
+                debug!("SUB ref");
                 let dest = fetch!(Byte);
                 let src = fetch!(Byte);
                 check_same_type!(dest, src);
@@ -928,7 +928,7 @@ impl<D: DiskController> CPUInternal<D> {
                 self.instruction_sub(dest, value)?;
             }
             0x27 => {  // SUBBORROW literal
-            debug!("SUBBORROW literal");
+                debug!("SUBBORROW literal");
                 let reg_ref = fetch!(Byte);
                 reject_float!(reg_ref);
                 let value = fetch_variable_size!(self.reg_ref_type(reg_ref)?);
@@ -937,7 +937,7 @@ impl<D: DiskController> CPUInternal<D> {
                 self.instruction_subborrow(reg_ref, value)?;
             }
             0x28 => {  // SUBBORROW ref
-            debug!("SUBBORROW ref");
+                debug!("SUBBORROW ref");
                 let dest = fetch!(Byte);
                 reject_float!(dest);
                 let src = fetch!(Byte);
@@ -948,14 +948,14 @@ impl<D: DiskController> CPUInternal<D> {
                 self.instruction_subborrow(dest, value)?;
             }
             0x29 => {  // MULT literal
-            debug!("MULT literal");
+                debug!("MULT literal");
                 let reg_ref = fetch!(Byte);
                 let value = fetch_variable_size!(self.reg_ref_type(reg_ref)?);
                 debug!("Multiplying register {:#x} by {:?}", reg_ref, value);
                 self.instruction_mult(reg_ref, value)?;
             }
             0x2A => {  // MULT ref
-            debug!("MULT ref");
+                debug!("MULT ref");
                 let dest = fetch!(Byte);
                 let src = fetch!(Byte);
                 check_same_type!(dest, src);
@@ -964,14 +964,14 @@ impl<D: DiskController> CPUInternal<D> {
                 self.instruction_mult(dest, value)?;
             }
             0x2B => {  // SDIV literal
-            debug!("SDIV literal");
+                debug!("SDIV literal");
                 let reg_ref = fetch!(Byte);
                 let value = fetch_variable_size!(self.reg_ref_type(reg_ref)?);
                 debug!("Signed dividing register {:#x} by {:?}", reg_ref, value);
                 self.instruction_sdiv(reg_ref, value)?;
             }
             0x2C => {  // SDIV ref
-            debug!("SDIV ref");
+                debug!("SDIV ref");
                 let dest = fetch!(Byte);
                 let src = fetch!(Byte);
                 check_same_type!(dest, src);
@@ -980,20 +980,52 @@ impl<D: DiskController> CPUInternal<D> {
                 self.instruction_sdiv(dest, value)?;
             }
             0x2D => {  // UDIV literal
-            debug!("UDIV literal");
+                debug!("UDIV literal");
                 let reg_ref = fetch!(Byte);
                 let value = fetch_variable_size!(self.reg_ref_type(reg_ref)?);
                 debug!("Unsigned dividing register {:#x} by {:?}", reg_ref, value);
                 self.instruction_udiv(reg_ref, value)?;
             }
             0x2E => {  // UDIV ref
-            debug!("UDIV ref");
+                debug!("UDIV ref");
                 let dest = fetch!(Byte);
                 let src = fetch!(Byte);
                 check_same_type!(dest, src);
                 let value = self.read_from_register(src)?;
                 debug!("Unsigned dividing register {:#x} by {:?}", dest, value);
                 self.instruction_udiv(dest, value)?;
+            }
+            0x2F => {  // SREM literal
+                debug!("SREM literal");
+                let reg_ref = fetch!(Byte);
+                let value = fetch_variable_size!(self.reg_ref_type(reg_ref)?);
+                debug!("Signed remainder register {:#x} by {:?}", reg_ref, value);
+                self.instruction_srem(reg_ref, value)?;
+            }
+            0x30 => {  // SREM ref
+                debug!("SREM ref");
+                let dest = fetch!(Byte);
+                let src = fetch!(Byte);
+                check_same_type!(dest, src);
+                let value = self.read_from_register(src)?;
+                debug!("Signed remainder register {:#x} by {:?}", dest, value);
+                self.instruction_srem(dest, value)?;
+            }
+            0x31 => {  // UREM literal
+            debug!("UREM literal");
+                let reg_ref = fetch!(Byte);
+                let value = fetch_variable_size!(self.reg_ref_type(reg_ref)?);
+                debug!("Unsigned remainder register {:#x} by {:?}", reg_ref, value);
+                self.instruction_urem(reg_ref, value)?;
+            }
+            0x32 => {  // UREM ref
+            debug!("UREM ref");
+                let dest = fetch!(Byte);
+                let src = fetch!(Byte);
+                check_same_type!(dest, src);
+                let value = self.read_from_register(src)?;
+                debug!("Unsigned remainder register {:#x} by {:?}", dest, value);
+                self.instruction_urem(dest, value)?;
             }
             0x70 => {  // SCONVERT
                 debug!("SCONVERT");
@@ -1106,7 +1138,7 @@ impl<D: DiskController> CPUInternal<D> {
         if self.flags & FLAG_CARRY != 0 {
             value.integer_add_one();
         }
-        bin_op_int!(self, reg_ref, value, overflowing_add, add)
+        bin_op_int!(self, reg_ref, value, overflowing_add)
     }
 
     fn instruction_sub(&mut self, reg_ref: u8, value: TypedValue) -> CPUResult<()> {
@@ -1119,7 +1151,7 @@ impl<D: DiskController> CPUInternal<D> {
         if self.flags & FLAG_CARRY != 0 {
             value.integer_add_one();
         }
-        bin_op_int!(self, reg_ref, value, overflowing_sub, sub)
+        bin_op_int!(self, reg_ref, value, overflowing_sub)
     }
 
     fn instruction_mult(&mut self, reg_ref: u8, value: TypedValue) -> CPUResult<()> {
@@ -1142,7 +1174,25 @@ impl<D: DiskController> CPUInternal<D> {
             self.interrupt_tx.send(INTERRUPT_DIV_BY_0).unwrap();
             return Err(CPUError);
         }
-        bin_op_int!(self, reg_ref, value, overflowing_div, div)
+        bin_op_int!(self, reg_ref, value, overflowing_div)
+    }
+
+    fn instruction_srem(&mut self, reg_ref: u8, value: TypedValue) -> CPUResult<()> {
+        // We assume that the value has already been checked to match the register type.
+        if value.is_integer_zero() {
+            self.interrupt_tx.send(INTERRUPT_DIV_BY_0).unwrap();
+            return Err(CPUError);
+        }
+        bin_op_signed!(self, reg_ref, value, overflowing_rem, rem)
+    }
+
+    fn instruction_urem(&mut self, reg_ref: u8, value: TypedValue) -> CPUResult<()> {
+        // We assume that the value has already been checked to match the register type.
+        if value.is_integer_zero() {
+            self.interrupt_tx.send(INTERRUPT_DIV_BY_0).unwrap();
+            return Err(CPUError);
+        }
+        bin_op_int!(self, reg_ref, value, overflowing_rem)
     }
 
     fn reg_ref_type(&self, reg_ref: u8) -> CPUResult<ValueType> {
@@ -2807,5 +2857,116 @@ mod tests {
         assert_eq!(internal!(cpu).r[1], 0x02);
         assert_eq!(internal!(cpu).r[2], 0x2D);
         assert_eq!(internal!(cpu).r[3], 0x00);
+    }
+
+    #[test]
+    #[timeout(100)]
+    fn test_srem() {
+        let mut rom = [0; 512];
+        rom[0] = 0x0A;  // Copy literal
+        rom[1] = 0x10;  // into r0b
+        rom[2] = 0x0F;  // 15.
+
+        rom[3] = 0x0A;  // Copy literal
+        rom[4] = 0x11;  // into r1b
+        rom[5] = 0x08;  // 8.
+
+        rom[6] = 0x30;  // Remainder register
+        rom[7] = 0x10;  // into r0b
+        rom[8] = 0x11;  // by r1b.
+
+        rom[9] = 0x2F;  // Remainder literal
+        rom[10] = 0x11; // into r1b
+        rom[11] = 0x04; // 4.
+
+        rom[12] = 0x0A; // Copy literal
+        rom[13] = 0x12; // into r2b.
+        rom[14] = 0xE2; // -30.
+
+        rom[15] = 0x2F; // Remainder literal
+        rom[16] = 0x12; // into r2b
+        rom[17] = 0x04; // 4.
+
+        rom[18] = 0x0A; // Copy literal
+        rom[19] = 0x13; // into r3b
+        rom[20] = 0xE2; // -30.
+
+        rom[21] = 0x2F; // Remainder literal
+        rom[22] = 0x13; // into r3b
+        rom[23] = 0xFC; // -4.
+
+        rom[24] = 0x0A; // Copy literal
+        rom[25] = 0x14; // into r4b
+        rom[26] = 0xFF; // 255.
+
+        rom[27] = 0x70; // Convert
+        rom[28] = 0x18; // into f0
+        rom[29] = 0x04; // r4.
+
+        rom[30] = 0x0A; // Copy literal
+        rom[31] = 0x14; // into r4b
+        rom[32] = 0x64; // 100.
+
+        rom[33] = 0x70; // Convert
+        rom[34] = 0x19; // into f1
+        rom[35] = 0x04; // r4.
+
+        rom[36] = 0x30; // Remainder register
+        rom[37] = 0x18; // into f0
+        rom[38] = 0x19; // by f1.
+
+        let (cpu, ui_commands) = run(rom, None);
+        assert_eq!(ui_commands.len(), 2);
+        assert_eq!(internal!(cpu).r[0], 0x07);
+        assert_eq!(internal!(cpu).r[1], 0x00);
+        assert_eq!(internal!(cpu).r[2], 0xFE);
+        assert_eq!(internal!(cpu).r[3], 0xFE);
+        assert_about_eq!(internal!(cpu).f[0], 55.0);
+        assert_about_eq!(internal!(cpu).f[1], 100.0);
+    }
+
+    #[test]
+    #[timeout(100)]
+    fn test_urem() {
+        let mut rom = [0; 512];
+        // These use the same values as test_srem, but interpret them as unsigned instead.
+        rom[0] = 0x0A;  // Copy literal
+        rom[1] = 0x10;  // into r0b
+        rom[2] = 0x0F;  // 15.
+
+        rom[3] = 0x0A;  // Copy literal
+        rom[4] = 0x11;  // into r1b
+        rom[5] = 0x08;  // 8.
+
+        rom[6] = 0x32;  // Remainder register
+        rom[7] = 0x10;  // into r0b
+        rom[8] = 0x11;  // r1b.
+
+        rom[9] = 0x31;  // Remainder literal
+        rom[10] = 0x11; // into r1b
+        rom[11] = 0x04; // 4.
+
+        rom[12] = 0x0A; // Copy literal
+        rom[13] = 0x12; // into r2b.
+        rom[14] = 0xE2; // 226.
+
+        rom[15] = 0x31; // Remainder literal
+        rom[16] = 0x12; // into r2b
+        rom[17] = 0x04; // 4.
+
+        rom[18] = 0x0A; // Copy literal
+        rom[19] = 0x13; // into r3b
+        rom[20] = 0xE2; // 226.
+
+        rom[21] = 0x31; // Remainder literal
+        rom[22] = 0x13; // into r3b
+        rom[23] = 0xFC; // 252.
+
+        let (cpu, ui_commands) = run(rom, None);
+        assert_eq!(ui_commands.len(), 2);
+        assert_eq!(internal!(cpu).r[0], 0x07);
+        assert_eq!(internal!(cpu).r[1], 0x00);
+        assert_eq!(internal!(cpu).r[2], 0x02);
+        assert_eq!(internal!(cpu).r[3], 0xE2);
     }
 }
