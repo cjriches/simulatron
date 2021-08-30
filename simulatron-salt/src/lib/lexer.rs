b980106af3c199e6c48c621dbec763fe6476a309
Chris Riches <chrisriches42@gmail.com>
2021-08-30 18:42:45 +0100

    SALT: Initial lexer implementation.

--- simulatron-salt/src/lib/lexer.rs
+++ simulatron-salt/src/lib/lexer.rs
@@ -1,15 +1,62 @@
 use logos::Logos;
 
-#[derive(Logos, Debug, PartialEq, Copy, Clone)]
+#[derive(Logos, Debug, PartialEq, Eq, Copy, Clone)]
 pub enum TokenType {
-    #[token(r"yeet")]
-    Yeet,
-
-    #[regex(r"\s+")]
-    Whitespace,
-
-    #[regex(r"[a-zA-Z]+")]
+    // Keywords
+    #[token("const")]
+    Const,
+    #[token("static")]
+    Static,
+    #[token("mut")]
+    Mut,
+    #[token("byte")]
+    Byte,
+    #[token("half")]
+    Half,
+    #[token("word")]
     Word,
+
+    // Punctuation
+    #[token("[")]
+    OpenSquare,
+    #[token("]")]
+    CloseSquare,
+    #[token(":")]
+    Colon,
+    #[token(",")]
+    Comma,
+    #[token("-")]
+    Minus,
+    #[token(".")]
+    Period,
+
+    // Literal components
+    #[regex(r"e-?[0-9]+")]
+    Exponent,
+    #[regex(r"[0-9]+")]
+    DecLiteral,
+    #[regex(r"0b[01]+")]
+    BinLiteral,
+    #[regex(r"0x[A-Fa-f0-9]+")]
+    HexLiteral,
+    #[regex("'\\\\n|\\\\\"|\\\\\\\\|[^\\n\\\\]'")]  // https://xkcd.com/1638/
+    CharLiteral,
+    #[regex("\"(\\\\n|\\\\\"|\\\\\\\\|[^\\n\\\\])*\"")]
+    StringLiteral,
+
+    // Identifiers
+    #[regex(r"[A-Za-z_][A-Za-z0-9_]*")]
+    Identifier,
+
+    // Comments
+    #[regex(r"//[^\n]*")]
+    Comment,
+
+    // Whitespace
+    #[regex(r"\r|\n|\r\n")]
+    Newline,
+    #[regex(r"[^\S\n\r]+")]
+    Whitespace,
 
     #[error]
     Error,
@@ -18,53 +65,33 @@ pub enum TokenType {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use super::TokenType::*;
 
-    use logos::Lexer;
+    use insta::assert_snapshot;
+    use std::fmt::Write;
 
-    #[derive(Debug, PartialEq, Copy, Clone)]
-    struct Token<'a> {
-        token_type: TokenType,
-        lexeme: &'a str,
-    }
-
-    fn assert_tokens(input: &str, tokens: &[Token]) {
-        let mut lex: Lexer<TokenType> = TokenType::lexer(input);
-        let mut pos: usize = 0;
-        for tok in tokens {
-            let end_pos: usize = pos + tok.lexeme.len();
-            assert_eq!(lex.next(), Some(tok.token_type));
-            assert_eq!(lex.span(), pos..end_pos);
-            assert_eq!(lex.slice(), tok.lexeme);
-            pos = end_pos;
+    fn assert_tokens_snapshot(path: &str) {
+        let input = std::fs::read_to_string(path).unwrap();
+        let mut lexer = TokenType::lexer(&input);
+        let mut output = String::new();
+        while let Some(t) = lexer.next() {
+            if let TokenType::Newline = t {
+                writeln!(output, "{:?}", t).unwrap();
+            } else {
+                writeln!(output, "{:?} '{}'", t, lexer.slice()).unwrap();
+            }
         }
+        assert_snapshot!(output);
     }
 
+    /// Test the simplest possible input: a single instruction.
     #[test]
-    fn test_simple() {
-        let input = "This is a sentence with the word yeet in it";
-        let space = Token {token_type: Whitespace, lexeme: " "};
-        let expected = [
-            Token {token_type: Word, lexeme: "This"},
-            space,
-            Token {token_type: Word, lexeme: "is"},
-            space,
-            Token {token_type: Word, lexeme: "a"},
-            space,
-            Token {token_type: Word, lexeme: "sentence"},
-            space,
-            Token {token_type: Word, lexeme: "with"},
-            space,
-            Token {token_type: Word, lexeme: "the"},
-            space,
-            Token {token_type: Word, lexeme: "word"},
-            space,
-            Token {token_type: Yeet, lexeme: "yeet"},
-            space,
-            Token {token_type: Word, lexeme: "in"},
-            space,
-            Token {token_type: Word, lexeme: "it"},
-        ];
-        assert_tokens(input, &expected);
+    fn test_minimal() {
+        assert_tokens_snapshot("examples/minimal.simasm");
+    }
+
+    /// Test a small instruction block.
+    #[test]
+    fn test_instruction_block() {
+        assert_tokens_snapshot("examples/instruction-block.simasm");
     }
 }
